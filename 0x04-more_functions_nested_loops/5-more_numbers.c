7b45a8114ebe62d1cbd681eaa8e99065b5ba05bc
Ane-Alx <abokhaiane.leadlearning@gmail.com>
2023-04-27 08:12:25 -0700

    I think the checker is broken

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* more_numbers - print more numbers
+*/
+
+void more_numbers(void)
+{
+	int i = 1;
+	int j = 0;
+
+	while (i <= 10)
+	{
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
