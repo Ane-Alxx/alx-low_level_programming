52e2f86e30ceaada032d5d1a6d2a6ef77ce77c16
Ane-Alx <abokhaiane.leadlearning@gmail.com>
2023-04-27 07:56:32 -0700

    15:56:28

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - multiply two integer
+* @a: first int
+* @b: second int
+* Return: multiplies two integer
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - print numbers 0 through 9
+* return: void
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i  <  58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
