c4c0c5514b9b0336699b6fd3784e688d513e363d
Ane-Alx <abokhaiane.leadlearning@gmail.com>
2023-05-02 07:05:43 -0700

    ------15:05:37-------

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_rev - prints a string, in reverse,
+* followed by a new line
+* @s: string
+* return: 0
+*/
+
+void print_rev(char *s)
+{
+	int count = 0;
+	int point;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	s--;
+	for (point = count; point > 0; point—)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
