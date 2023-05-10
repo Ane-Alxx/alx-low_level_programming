2b1193bd8c6aa989a859734302a752816aa68e56
Ane-Alx <abokhaiane.leadlearning@gmail.com>
2023-05-10 02:32:02 -0700

    First

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts_recursion - function for _puts_recursion
+* @s: input
+* Return: Always 0 (Success)
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s != NULL)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
